f7834020c73fd438416d2e9595554574eddadacd
toshiba <toshiba@PC>
2019-11-04 04:00:13 +0500

    pushhs

---
+++ faizan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace faizan
+{
+    class pets
+    {
+
+        public string name = "cat";
+        public int age = 12;
+        public float happiness = 0.5f;
+        public void print()
+        {
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("age:" + age);
+            Console.WriteLine("happiness:" + happiness);
+        }
+        class Mainclass
+        {
+            public static void Main(string[] args)
+            {
+                pets dog = new pets();
+                dog.print();
+                Console.ReadKey();
+            }
+
+
+
+
+        }
+    }
+}
